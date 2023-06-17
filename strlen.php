1200dd18cec08e0a452b1e5a8f5280cba5ad3193
Dishant Dilip Yashwante <113447476+dishant1996@users.noreply.github.com>
2023-06-18 00:31:14 +0530

    string commands

---
+++ strlen.php
@@ -0,0 +1,7 @@
+<?php
+
+$a = 'dishant';
+
+echo strlen($a);
+
+?>                     
---
+++ strtupper.php
@@ -0,0 +1,17 @@
+<?php
+//TURNING IN UPPER CASE
+$str = "dishant";
+echo strtoupper($str);
+
+
+?>
+
+
+<?php
+
+$str = "DISHANT";
+echo strtolower($str);
+
+
+
+?>
